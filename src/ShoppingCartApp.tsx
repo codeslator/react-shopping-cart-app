a1d456feee185c7c5d78cde63babd13357d90df2
Andrés Meléndez <codeslator@gmail.com>
2024-10-28 20:08:28 -0400

    Changes: Add Chakra UI to create ui layout; Add chakra component snippets; Configure paths in tsconfig and vite.config;;

--- src/ShoppingCartApp.tsx
+++ src/ShoppingCartApp.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@/components/ui/button"
+
 function ShoppingCartApp() {
 
   return (
     <>
       <h1>Hello world</h1>
+      <Button colorPalette="teal" variant="subtle">Click me</Button>
     </>
   )
 }
