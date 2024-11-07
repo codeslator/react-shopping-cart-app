670fa7f08f229d5ca3c729e62ae6462310c55f0a
Andrés Meléndez <codeslator@gmail.com>
2024-11-06 20:22:58 -0400

    changes: Add headers to supabase api instance;

--- src/apis/supabaseApi.ts
+++ src/apis/supabaseApi.ts
@@ -2,5 +2,9 @@ import axios, { AxiosInstance } from 'axios';
 import { environment } from '@/global';
 
 export const supabaseApi: AxiosInstance = axios.create({
-  baseURL: `${environment.SUPABASE_API_URL}/rest/v1`
+  baseURL: `${environment.SUPABASE_API_URL}/rest/v1`,
+  headers: {
+    'Authorization': `Bearer ${environment.SUPABASE_API_KEY}`,
+    'apikey': environment.SUPABASE_API_KEY
+  }
 });
